6da3233f5cb31ff96e1690d91da8ee335dee90ab
poorna99122 <46306217+poorna99122@users.noreply.github.com>
2024-08-07 11:01:37 +0530

    form controls added

--- src/app/user-login/user-login.component.ts
+++ src/app/user-login/user-login.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { User } from '../user';
 
 @Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
-  styleUrls: ['./user-login.component.scss']
+  styleUrls: ['./user-login.component.scss'],
 })
 export class UserLoginComponent {
+  user : User = new User()
+  constructor() {}
 
+  ngOnInit() {
+    console.log('User Login Component Initiated');
+  }
+
+  userLogin(){
+    console.log("userLoggedIn");
+    console.log(this.user);
+    
+    
+  }
 }
